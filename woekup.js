3508055d8172ea829fa1b725d4170fd972663062
ROYCE WHELAN <royce.whelan@gmail.com>
2014-01-14 16:37:46 -0500

    forgot to commit

--- woekup.js
+++ woekup.js
@@ -1,13 +1,40 @@
 var http = require('http')
+  //, appid = require('./yahoo_appid.json')
+  //, fs = require('fs')
   , AWS = require('aws-sdk')
-AWS.config.loadFromPath('./aws_auth.json')
-AWS.config.update({region: 'us-east-1'})
 
-var appid
+AWS.config.loadFromPath('aws_auth.json')
+//AWS.config.update({region: 'us-east-1'})
 
-http://where.yahooapis.com/v1/countries?appid=[yourappidhere]
-http.get("http://www.google.com/index.html", function(res) {
+var s3 = new AWS.S3()
+
+
+s3.listBuckets().
+	on('success', function(res){
+		console.log("success")
+		console.log(res)
+	}).
+	on('error', function(res){
+		console.log("I am error.")
+		console.log(res)
+	}).
+	on('complete', function(){
+		console.log("complete")
+	}).
+	send()
+
+
+
+
+/*
+http.get('http://where.yahooapis.com/v1/countries?appid='+appid, function(res) {
+
+	var filename = 'countries.json'
+	  , outstring = JSON.stringify(res, null, '\t')
+	fs.writeFile()
+
 	console.log("Got response: " + res.statusCode);
 }).on('error', function(e) {
 	console.log("Got error: " + e.message);
 });
+*/
